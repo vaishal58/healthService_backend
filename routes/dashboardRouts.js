6ff30ff67b1f7a177d99a0e90a9104071f57f249
shalin sheth <shalinsheth4915@gmail.com>
2023-10-18 11:08:16 +0530

    dashboard changes

--- routes/dashboardRouts.js
+++ routes/dashboardRouts.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { getDashboardData } = require("../controllers/dashboardController");
 const router = express.Router();
 
-router.post("/get-dashboard-data" , getDashboardData);
+router.get("/get-dashboard-data" , getDashboardData);
 
 module.exports = router;
 
