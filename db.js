321f606c9a411ed6408b516b2c94c7fdea0d14da
shalin sheth <shalinsheth4915@gmail.com>
2023-11-29 11:49:26 +0530

    New Database Link Added

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const connectToMongo = ()=>{
-    mongoose.connect("mongodb+srv://barodaweb:Barodaweb-mongo2022@cluster0.jruibih.mongodb.net/PushtiShangar_database?retryWrites=true&w=majority").then(()=>{
+    mongoose.connect("mongodb+srv://barodaweb:Barodaweb-mongo2022@cluster0.jruibih.mongodb.net/VirajHospital_database?retryWrites=true&w=majority").then(()=>{
         console.log("connection successful"); 
     })
 }
